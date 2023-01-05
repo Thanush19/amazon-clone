6eae5e32404ba43c6ea84cd236efaa307eceba53
Simon <that.thanush.the.bloggy@gmail.com>
2023-01-05 13:16:04 +0530

    first commit

---
+++ firebase.js
@@ -0,0 +1,13 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBHL1g2sv6BKZvdQfbKeBJG2b7pVeqh3m8",
+    authDomain: "clone-5dcef.firebaseapp.com",
+    projectId: "clone-5dcef",
+    storageBucket: "clone-5dcef.appspot.com",
+    messagingSenderId: "135031617907",
+    appId: "1:135031617907:web:54368d4d89b2571a01ad06",
+    measurementId: "G-S1C12YWYHY"
+  };
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
